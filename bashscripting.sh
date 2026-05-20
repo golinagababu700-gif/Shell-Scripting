90201c845dd53572378472f011a9ed490b4b5aa1
nagababugoli <golinagababu700@gmail.com>
2026-05-20 01:55:23 -0500

    updated code

---
+++ bashscripting.sh
@@ -0,0 +1,22 @@
+ #!/bin/bash
+
+echo "Naga Babu"
+lovername="Teja Sri"
+
+echo "nagababu lovername is  "$lovername
+
+
+
+age=27
+if [ $age -ge 18 ]
+then
+	echo "he is major"
+else
+	echo "he is minor"
+fi
+
+date
+time
+echo "enter surname:"
+read surname
+echo $surname
---
+++ gitinitialcommand.sh
@@ -0,0 +1,7 @@
+# for initial repository shell scripting commands 
+git init
+git add .
+git commit -m "updated code"
+git branch -M main
+git remote add origin https://github.com/golinagababu700-gif/Shell-Scripting.git
+git push -u origin main
